7d15a2384bbdd3dccad8e249894fe64de0b9ffb0
SamHHS <samcinjee@gmail.com>
2021-02-28 19:47:57 +0100

    Chart fixes
    Added colors, changed labels, updated data

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// window.onload = function(){
-//         loadChart();
-// }
+window.onload = function(){
+        loadChart();
+}
 
 function loadChart(){
-    var ctx = document.getElementById('myChartSpeed').getContext('2d');
+    var ctx = document.getElementById('waterUsage').getContext('2d');
     var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['1st Quarter', '2nd Quarter', '3rd Quarter', '4th Quarter'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Water used',
+            data: [12, 19, 3, 5],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -41,13 +41,13 @@ function loadChart(){
     }
 });
 
- var ctx = document.getElementById('myChartFood').getContext('2d');
+ var ctx = document.getElementById('fruitUsage').getContext('2d');
     var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['12', '19', '3', '5', '2', '3'],
         datasets: [{
-            label: '# of Votes',
+            label: 'Fruit consumed a day',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -57,14 +57,7 @@ function loadChart(){
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+           
             borderWidth: 1
         }]
     },
@@ -79,31 +72,24 @@ function loadChart(){
     }
 });
 
-     var ctx = document.getElementById('myChartWater').getContext('2d');
+     var ctx = document.getElementById('fruitSupply').getContext('2d');
     var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Apple', 'Banana', 'Pear', 'Orange', 'Melon', 'Grapefruit'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'fruit in storage(KG)',
+            data: [120, 190, 30, 50, 20, 30],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                '#ff0800',
+                '#ffd429',
+                '#d1e231',
+                '#ffa500',
+                '#fdbcb4',
+                'fd5956'
             ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            segmentShowStroke : false,
+            borderWidth: 1, 
         }]
     },
     options: {
